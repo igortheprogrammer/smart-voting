ba4822c1a9dbdf51e1e678356aa40198870f2f68
Igor Inozemtsev <igortheprogrammer@gmail.com>
2022-03-24 11:07:01 +0300

    finishVoting method

--- contracts/SmartVoting.sol
+++ contracts/SmartVoting.sol
@@ -4,14 +4,48 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract SmartVoting {
+    struct Vote {
+        address candidate;
+        address voter;
+    }
+
     struct Voting {
         uint256 id;
         string title;
         uint256 endTime;
         bool finished;
+        address winner;
+        address[] candidates;
+        Vote[] votes;
+        mapping(address => address[]) candidateVotes;
+        mapping(address => bool) voters;
     }
 
-    event VotingAdded(uint256 id, string title, uint256 endTime);
+    struct VotingStats {
+        uint256 id;
+        string title;
+        uint256 endTime;
+        bool finished;
+        address[] candidates;
+        address winner;
+        Vote[] votes;
+    }
+
+    event VotingAdded(
+        uint256 id,
+        string title,
+        uint256 endTime,
+        address[] candidates
+    );
+
+    event VotingFinished(
+        uint256 id,
+        string title,
+        uint256 endTime,
+        address winner
+    );
+
+    event VoteAdded(uint256 id, address voter, address candidate);
 
     address private owner;
 
@@ -20,10 +54,47 @@ contract SmartVoting {
     mapping(uint256 => Voting) private votings;
 
     /// Only the owner can call this function.
-    error onlyOwnerErr();
+    error NotOwner();
+
+    /// Voting does not exist.
+    error VotingDoesNotExist();
+
+    /// This voting already finished.
+    error VoteAlreadyFinished();
+
+    /// Candidate does not exist.
+    error CandidateDoesNotExist();
+
+    /// This vote already counted at voting.
+    error VoteAlreadyCounted();
+
+    /// Correct bid is 0.01 ETH.
+    error IncorrectBid();
 
     modifier onlyOwner() {
-        if (msg.sender != owner) revert onlyOwnerErr();
+        if (msg.sender != owner) revert NotOwner();
+        _;
+    }
+
+    modifier votingExists(uint256 _votingId) {
+        if (votings[_votingId].endTime == 0) revert VotingDoesNotExist();
+        _;
+    }
+
+    modifier votingIsActive(uint256 _votingId) {
+        if (votings[_votingId].finished) revert VoteAlreadyFinished();
+        _;
+    }
+
+    modifier candidateExists(uint256 _votingId, address _candidate) {
+        bool found = false;
+        for (uint256 i = 0; i < votings[_votingId].candidates.length; i++) {
+            if (votings[_votingId].candidates[i] == _candidate) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) revert CandidateDoesNotExist();
         _;
     }
 
@@ -31,25 +102,126 @@ contract SmartVoting {
         owner = msg.sender;
     }
 
-    function addVoting(string memory title) external onlyOwner {
-        uint256 endTime = block.timestamp + 3 days;
-        votings[currentId] = Voting({
-            id: currentId,
-            title: title,
-            endTime: endTime,
-            finished: false
-        });
+    function addVoting(
+        uint256 _duration,
+        string memory _title,
+        address[] memory _candidates
+    ) external onlyOwner {
+        require(
+            keccak256(abi.encodePacked(_title)) !=
+                keccak256(abi.encodePacked("")),
+            "Voting title is required."
+        );
 
-        emit VotingAdded(currentId, title, endTime);
+        require(_candidates.length > 0, "Candidates are required.");
+
+        uint256 endTime = block.timestamp + _duration;
+        Voting storage v = votings[currentId];
+        v.id = currentId;
+        v.title = _title;
+        v.endTime = endTime;
+        v.finished = false;
+        v.candidates = _candidates;
+
+        emit VotingAdded(currentId, _title, endTime, _candidates);
 
         currentId++;
     }
 
-    function getVotings() public view returns (Voting[] memory) {
-        Voting[] memory res = new Voting[](currentId);
-        for (uint i = 0; i < currentId; i++) {
-            res[i] = votings[i];
+    function getVotings() public view returns (VotingStats[] memory) {
+        VotingStats[] memory res = new VotingStats[](currentId);
+        for (uint256 i = 0; i < currentId; i++) {
+            res[i] = VotingStats({
+                id: votings[i].id,
+                title: votings[i].title,
+                endTime: votings[i].endTime,
+                finished: votings[i].finished,
+                winner: votings[i].winner,
+                candidates: votings[i].candidates,
+                votes: votings[i].votes
+            });
         }
         return res;
+    }
+
+    function finishVoting(uint256 _votingId)
+        external
+        votingExists(_votingId)
+        votingIsActive(_votingId)
+    {
+        require(
+            votings[_votingId].endTime <= block.timestamp,
+            "Voting hasn't ended yet."
+        );
+
+        address currentCandidate = votings[_votingId].candidates[0];
+        uint256 maxVotes = votings[_votingId]
+            .candidateVotes[currentCandidate]
+            .length;
+        uint256 num = 1;
+        for (uint256 i = 1; i < votings[_votingId].candidates.length; i++) {
+            currentCandidate = votings[_votingId].candidates[i];
+            if (
+                votings[_votingId].candidateVotes[currentCandidate].length >
+                maxVotes
+            ) {
+                maxVotes = votings[_votingId]
+                    .candidateVotes[currentCandidate]
+                    .length;
+                num = 1;
+            } else if (
+                votings[_votingId].candidateVotes[currentCandidate].length ==
+                maxVotes
+            ) {
+                num++;
+            }
+        }
+
+        address[] memory leaders = new address[](num);
+        uint256 pos = 0;
+        for (uint256 i = 0; i < votings[_votingId].candidates.length; i++) {
+            currentCandidate = votings[_votingId].candidates[i];
+            if (
+                votings[_votingId].candidateVotes[currentCandidate].length ==
+                maxVotes
+            ) {
+                leaders[pos] = currentCandidate;
+                pos++;
+            }
+        }
+
+        uint256 randomIndex = uint256(
+            keccak256(abi.encodePacked(block.timestamp, msg.sender))
+        ) % leaders.length;
+
+        votings[_votingId].winner = leaders[randomIndex];
+        votings[_votingId].finished = true;
+
+        emit VotingFinished(
+            _votingId,
+            votings[_votingId].title,
+            block.timestamp,
+            votings[_votingId].winner
+        );
+    }
+
+    function addVote(uint256 _votingId, address _candidate)
+        external
+        payable
+        votingExists(_votingId)
+        votingIsActive(_votingId)
+        candidateExists(_votingId, _candidate)
+    {
+        if (msg.value != 10000000 gwei) revert IncorrectBid();
+
+        if (votings[_votingId].voters[msg.sender]) revert VoteAlreadyCounted();
+
+        votings[_votingId].voters[msg.sender] = true;
+        votings[_votingId].votes.push(
+            Vote({candidate: _candidate, voter: msg.sender})
+        );
+        votings[_votingId].candidateVotes[_candidate].push(msg.sender);
+
+        emit VoteAdded(_votingId, msg.sender, _candidate);
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -2,6 +2,7 @@ import { ethers } from 'hardhat';
 import { expect } from 'chai';
 
 import { SmartVoting } from '../typechain';
+import * as timers from 'timers';
 
 describe('SmartVoting', function() {
   let contract: SmartVoting;
@@ -12,28 +13,406 @@ describe('SmartVoting', function() {
     await contract.deployed();
   });
 
-  it('Should add new voting from the owner', async function() {
-    const [owner] = await ethers.getSigners();
-    const result = await contract.connect(owner).addVoting('test 1');
-    expect(result.from).equal(owner.address);
+  describe('addVoting method', function() {
+    it('should add new voting from the owner', async function() {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+      const result = await contract
+        .connect(owner)
+        .addVoting(
+          60,
+          'test 1',
+          [owner.address, addr1.address, addr2.address]
+        );
+      expect(result.from).equal(owner.address);
+    });
+
+    it('should not add new voting from a non-owner', async function() {
+      const [_, nonOwner] = await ethers.getSigners();
+      await expect(contract.connect(nonOwner)
+        .addVoting(
+          60,
+          'test 2',
+          [nonOwner.address]
+        ))
+        .to
+        .be
+        .revertedWith(`VM Exception while processing transaction: reverted with custom error 'NotOwner()'`);
+    });
+
+    it('should not add new voting without title', async function() {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+      await expect(contract
+        .addVoting(
+          60,
+          '',
+          [owner.address, addr1.address, addr2.address]
+        ))
+        .to
+        .be
+        .revertedWith(`VM Exception while processing transaction: reverted with reason string 'Voting title is required.'`);
+    });
+
+    it('should not add new voting without candidates', async function() {
+      await expect(contract
+        .addVoting(
+          60,
+          'test 4',
+          []
+        ))
+        .to
+        .be
+        .revertedWith(`VM Exception while processing transaction: reverted with reason string 'Candidates are required.'`);
+    });
   });
 
-  it('Should not add new voting from a non-owner', async function() {
-    const [_, nonOwner] = await ethers.getSigners();
-    await expect(contract.connect(nonOwner)
-      .addVoting('test 2'))
-      .to
-      .be
-      .revertedWith(`VM Exception while processing transaction: reverted with custom error 'onlyOwnerErr()'`);
+  describe('getVotings method', function() {
+    it('should returns votings with candidates', async function() {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+      await contract.addVoting(60, 'test 1', [addr1.address]);
+      await contract.addVoting(60, 'test 2', [addr2.address]);
+      await contract.addVoting(60, 'test 3', [owner.address]);
+      const result = await contract.getVotings();
+      expect(result.length).equal(3);
+      expect(result[0].title).equal('test 1');
+      expect(result[2].id.toNumber()).equal(2);
+      expect(result[1].candidates[0]).equal(addr2.address);
+    });
   });
 
-  it('Should returns votings', async function() {
-    await contract.addVoting('test 1');
-    await contract.addVoting('test 2');
-    await contract.addVoting('test 3');
-    const result = await contract.getVotings();
-    expect(result.length).equal(3);
-    expect(result[0].title).equal('test 1');
-    expect(result[2].id.toNumber()).equal(2);
+  describe('addVote method', function() {
+    it('should add vote to voting', async function() {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+      await contract
+        .addVoting(60, 'test 1', [owner.address, addr1.address]);
+
+      await contract
+        .connect(addr2)
+        .addVote(
+          0,
+          addr1.address,
+          {
+            value: ethers.utils.parseEther('0.01')
+          }
+        );
+
+      const result = await contract.getVotings();
+      expect(result[0].votes[0].voter).equal(addr2.address);
+    });
+
+    it('should return error if voting does not exist', async function() {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+      await expect(contract
+        .connect(addr2)
+        .addVote(
+          0,
+          addr1.address,
+          {
+            value: ethers.utils.parseEther('0.01')
+          }
+        ))
+        .to
+        .be
+        .revertedWith(`VM Exception while processing transaction: reverted with custom error 'VotingDoesNotExist()'`);
+    });
+
+    it('should return error if candidate does not exist', async function() {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+      await contract
+        .addVoting(60, 'test 1', [owner.address, addr1.address]);
+
+      await expect(contract
+        .connect(addr2)
+        .addVote(
+          0,
+          addr2.address,
+          {
+            value: ethers.utils.parseEther('0.01')
+          }
+        ))
+        .to
+        .be
+        .revertedWith(`VM Exception while processing transaction: reverted with custom error 'CandidateDoesNotExist()'`);
+    });
+
+    it('should return error if bid is incorrect', async function() {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+      await contract
+        .addVoting(60, 'test 1', [owner.address, addr1.address]);
+
+      await expect(contract
+        .connect(addr2)
+        .addVote(
+          0,
+          addr1.address,
+          {
+            value: ethers.utils.parseEther('0.001')
+          }
+        ))
+        .to
+        .be
+        .revertedWith(`VM Exception while processing transaction: reverted with custom error 'IncorrectBid()'`);
+    });
+
+    it('should return error if vote already counted', async function() {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+      await contract
+        .addVoting(60, 'test 1', [owner.address, addr1.address]);
+
+      await contract
+        .connect(addr2)
+        .addVote(
+          0,
+          addr1.address,
+          {
+            value: ethers.utils.parseEther('0.01')
+          }
+        );
+
+      await expect(contract
+        .connect(addr2)
+        .addVote(
+          0,
+          addr1.address,
+          {
+            value: ethers.utils.parseEther('0.01')
+          }
+        ))
+        .to
+        .be
+        .revertedWith(`VM Exception while processing transaction: reverted with custom error 'VoteAlreadyCounted()'`);
+    });
+  });
+
+  describe('finishVoting method', function() {
+    it('should finish the voting with the correct winner', async function() {
+      const [owner, addr1, addr2, addr4] = await ethers.getSigners();
+      await contract
+        .addVoting(
+          1,
+          'test 1',
+          [owner.address, addr1.address, addr2.address]
+        );
+      await contract
+        .addVoting(
+          60,
+          'test 2',
+          [addr1.address, addr2.address]
+        );
+
+      await contract
+        .connect(owner)
+        .addVote(
+          0,
+          owner.address,
+          {
+            value: ethers.utils.parseEther('0.01')
+          }
+        );
+      await contract
+        .connect(addr2)
+        .addVote(
+          0,
+          addr2.address,
+          {
+            value: ethers.utils.parseEther('0.01')
+          }
+        );
+      await contract
+        .connect(addr1)
+        .addVote(
+          0,
+          addr1.address,
+          {
+            value: ethers.utils.parseEther('0.01')
+          }
+        );
+      await contract
+        .connect(addr4)
+        .addVote(
+          0,
+          addr1.address,
+          {
+            value: ethers.utils.parseEther('0.01')
+          }
+        );
+
+      return new Promise((resolve, reject) => {
+        setTimeout(async function() {
+          contract.finishVoting(0)
+            .then(() => contract.getVotings())
+            .then((result) => {
+              expect(result[0].finished).to.equal(true);
+              expect(result[0].winner).to.equal(addr1.address);
+              resolve();
+            })
+            .catch((e) => reject(e));
+        }, 1000);
+      });
+    });
+
+    describe('finishVoting method', function() {
+      it('should finish the voting with the random winner', async function() {
+        const signers = await ethers.getSigners();
+        const [owner, addr1, addr2, addr4, addr5] = signers;
+        await contract
+          .addVoting(
+            1,
+            'test 1',
+            [owner.address, addr1.address, addr2.address]
+          );
+
+        await contract
+          .connect(owner)
+          .addVote(
+            0,
+            owner.address,
+            {
+              value: ethers.utils.parseEther('0.01')
+            }
+          );
+        await contract
+          .connect(addr5)
+          .addVote(
+            0,
+            owner.address,
+            {
+              value: ethers.utils.parseEther('0.01')
+            }
+          );
+        await contract
+          .connect(addr2)
+          .addVote(
+            0,
+            addr2.address,
+            {
+              value: ethers.utils.parseEther('0.01')
+            }
+          );
+        await contract
+          .connect(addr1)
+          .addVote(
+            0,
+            addr1.address,
+            {
+              value: ethers.utils.parseEther('0.01')
+            }
+          );
+        await contract
+          .connect(addr4)
+          .addVote(
+            0,
+            addr1.address,
+            {
+              value: ethers.utils.parseEther('0.01')
+            }
+          );
+
+        return new Promise((resolve, reject) => {
+          setTimeout(async function() {
+            contract.finishVoting(0)
+              .then(() => contract.getVotings())
+              .then((result) => {
+                expect(result[0].finished).to.equal(true);
+                expect(signers
+                  .find(({ address }) => address === result[0].winner))
+                  .not
+                  .to
+                  .be
+                  .undefined;
+                resolve();
+              })
+              .catch((e) => reject(e));
+          }, 1000);
+        });
+      });
+
+      it('should return error if it is too early', async function() {
+        const [owner, addr1, addr2] = await ethers.getSigners();
+        await contract
+          .addVoting(
+            60,
+            'test 1',
+            [addr1.address, addr2.address]
+          );
+
+        await contract
+          .connect(owner)
+          .addVote(
+            0,
+            addr1.address,
+            {
+              value: ethers.utils.parseEther('0.01')
+            }
+          );
+        await contract
+          .connect(addr2)
+          .addVote(
+            0,
+            addr2.address,
+            {
+              value: ethers.utils.parseEther('0.01')
+            }
+          );
+
+        await expect(contract
+          .finishVoting(0))
+          .to
+          .be
+          .revertedWith(`VM Exception while processing transaction: reverted with reason string 'Voting hasn't ended yet.'`);
+      });
+
+      it('should return error if the voting is already finished', async function() {
+        const [owner, addr1, addr2] = await ethers.getSigners();
+        await contract
+          .addVoting(
+            1,
+            'test 1',
+            [addr1.address, addr2.address]
+          );
+
+        await contract
+          .connect(owner)
+          .addVote(
+            0,
+            addr1.address,
+            {
+              value: ethers.utils.parseEther('0.01')
+            }
+          );
+        await contract
+          .connect(addr1)
+          .addVote(
+            0,
+            addr1.address,
+            {
+              value: ethers.utils.parseEther('0.01')
+            }
+          );
+        await contract
+          .connect(addr2)
+          .addVote(
+            0,
+            addr2.address,
+            {
+              value: ethers.utils.parseEther('0.01')
+            }
+          );
+
+        return new Promise((resolve, reject) => {
+          setTimeout(async function() {
+            try {
+              await contract.finishVoting(0);
+              await expect(contract.finishVoting(0))
+                .to
+                .be
+                .revertedWith(`VM Exception while processing transaction: reverted with custom error 'VoteAlreadyFinished()'`);
+              resolve();
+            } catch (e) {
+              reject(e);
+            }
+          }, 1000);
+        });
+      });
+    });
   });
 });
